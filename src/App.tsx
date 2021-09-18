e7f83a389cbd809f7cb05512898869b5dced0273
Hrushikesh Makode <makodehrushikesh@gmail.com>
2021-09-18 09:04:52 +0530

    add svgs and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Todos from "./components/Todos";
 
 function App() {
-    return (
-        <div className="h-screen flex justify-center items-center bg-gray-100">
-            <Todos />
-        </div>
-    );
+	return (
+		<div className="h-screen flex justify-center items-center bg-gray-100">
+			<Todos />
+		</div>
+	);
 }
 
 export default App;
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,17 +1,1 @@
-export const data = [
-    {
-        id: "0",
-        task: "Do Somethign 1",
-        isCompleted: false,
-    },
-    {
-        id: "1",
-        task: "Do Somethign 2",
-        isCompleted: false,
-    },
-    {
-        id: "2",
-        task: "Do Somethign 3",
-        isCompleted: false,
-    },
-];
+export const data = [];
